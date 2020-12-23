00ae86c642e87e58d797f268e2298f56675b7412
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-23 11:42:49 -0600

    OESS-104:Create szip repo.

---
+++ config/cmake/cacheinit.cmake
@@ -0,0 +1,17 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+set (BUILD_SHARED_LIBS ON CACHE BOOL "Build Shared Libraries" FORCE)
+
+set (CMAKE_INSTALL_FRAMEWORK_PREFIX "Library/Frameworks" CACHE STRING "Frameworks installation directory" FORCE)
+
+set (SZIP_PACKAGE_EXT "" CACHE STRING "Name of package extension" FORCE)
+
+set (PACKAGE_NAMESPACE "szip::" CACHE STRING "Name for package namespace" FORCE)
+
+set (BUILD_TESTING ON CACHE BOOL "Build SZIP Unit Testing" FORCE)
+
+set (SZIP_ENABLE_ENCODING ON CACHE BOOL "Enable SZIP Encoding" FORCE)
